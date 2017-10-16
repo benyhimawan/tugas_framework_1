5877c7a625a93869604430d528b22f5cc3bc8e35
benyhimawan <beny.himawan93@gmail.com>
2017-10-16 12:47:57 +0700

    framework_1

---
+++ tugas_framwork1.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link rel="stylesheet" href="style.scss">
+
+<div class="container">
+  <div class="row">
+  <div class="col-5 col1"></div>
+    <div class="col-2 col2"></div>
+    <div class="col-5 col3"></div>
+    <div class="w-100"></div>
+    <div class="col-4 col4"></div>
+    <div class="col-4 col5"></div>
+    <div class="col-4 col6"></div>
+    </div>
+    <div class="row">
+    <div class="col-3 col7"></div>
+    <div class="col-6 col8"></div>
+    <div class="col-3 col9"></div>
+    <div class="w-100"></div>
+    <div class="col-2 col10"></div>
+    <div class="col-8 col11"></div>
+    <div class="col-2 col12"></div>
+    </div>
+  <div class="row">
+    <div class="col-12 col13"></div>
+    <div class="w-100"></div>
+    <div class="col-2 col10"></div>
+    <div class="col-8 col11"></div>
+    <div class="col-2 col12"></div>
+    </div>
+    <div class="row">
+    <div class="col-3 col7"></div>
+    <div class="col-6 col8"></div>
+    <div class="col-3 col9"></div>
+    <div class="w-100"></div>
+    <div class="col-4 col4"></div>
+    <div class="col-4 col5"></div>
+    <div class="col-4 col6"></div>
+</div>
+<div class="row">
+  <div class="col-5 col1"></div>
+    <div class="col-2 col2"></div>
+    <div class="col-5 col3"></div>
+</div>
+</div>
+    
+  
